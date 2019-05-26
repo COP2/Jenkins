237908b469fc4ba2eecbf4cb560ca20189ebd1b6
COP2 <47363987+COP2@users.noreply.github.com>
2019-05-26 16:28:18 +0400

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,19 +1,5 @@
-import javax.swing.*;
-import java.awt.*;
-public class HelloWorld extends JFrame {
+public class HelloWorld {
     public static void main(String[] args) {
-        new HelloWorld();
-    }
-    HelloWorld() {
-        setTitle(sayHello());
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setBounds(200, 200, 300, 200);
-        JLabel label = new JLabel(sayHello(), SwingConstants.CENTER);
-        label.setFont(new Font("", Font.BOLD, 24));
-        add(label);
-        setVisible(true);
-    }
-    String sayHello() {
-        return "Hello, World!";
+        System.out.println("Hello, World!");
     }
 }
